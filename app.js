2b935ce0c65a0f3f60274da52fe4b952de84985e
Krrish-Devani <krrishdevani84@gmail.com>
2025-06-06 16:31:35 +0530

    WanderLust

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}/listings`);
 });
